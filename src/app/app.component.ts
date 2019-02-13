762d910fe7da3c902c369b5f0656937e47649225
Éoghan Muldoon <37183734+EoghanMuldoon@users.noreply.github.com>
2019-02-13 15:48:28 +0000

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'data-binding-app';
+  counter: number = 0;
+  hide: boolean = false;
+
+
+  OnButtonClick() {
+    console.log("Button");
+    this.counter++;
+  }
+
+  Doubleclick() {
+    console.log("double");
+    if (this.hide == false) {
+      this.hide = true;
+    }
+    else {
+      this.hide = false;
+    }
+  }
 }
